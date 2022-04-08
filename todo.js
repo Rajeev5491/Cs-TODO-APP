26e9478b9ded460b9fb1e645042902687e619d36
Rajeev S Nair <101646894+Rajeev5491@users.noreply.github.com>
2022-04-08 20:47:09 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,31 @@
+
+function login(form) {
+    
+    if ( form.user.value=="admin" && form.pass.value=="12345") { 
+      $('.login').fadeOut(1000);
+      $('.website').fadeIn(1000);
+      var user = document.getElementById("user").value;
+      
+    } else {
+      alert("Invalid Login")
+           }
+    }
+  
+  $('button').click(function(){
+    $('.login').fadeIn(1000);
+    $('.website').fadeOut(1000);
+    document.forms[0].reset();
+  });
+
+
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+
+
+  // for  todoo
+
+
+
+  
